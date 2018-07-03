b34bef8bb90d06dc65acc5555e53b5135915acd1
Santiago Alvarez-Vargas <santiago.v.alvarez@gmail.com>
2018-07-03 04:11:49 -0400

    Scaffolded Tiffin application

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    redirect_to root_path
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root   'static_pages#home'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+end
