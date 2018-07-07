477942baf6aaad8d03578f62af6241d4cc3f9560
Samir Reddigari <samirreddigari@gmail.com>
2018-07-07 17:01:47 -0400

    Alter chef route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users do
-    resource :chefs
+    resource :chef
   end
 end
