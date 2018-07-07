357e93bd902085f5d9d049e7e8aed39226d465de
Chris Auclair <chris.auclair@gmail.com>
2018-07-07 16:11:34 -0400

    test sessions helper login

---
+++ spec/helpers/sessions_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe SessionsHelper do
+
+  describe "#log_in" do
+    let(:user) {create(:user)}
+
+    before {log_in(user)}
+
+    it 'should store user id in session' do
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it 'should store last login time in session' do
+      expect(session[:last_login_time]).to be_present
+    end
+
+    it 'should store last login time in session' do
+      expect(session[:last_request_time]).to be_present
+    end
+
+  end
+
+  describe "#log_out" do
+
+  end
+
+end
