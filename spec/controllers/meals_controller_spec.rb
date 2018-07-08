ed90ea28b413bb6d9fab721610e23f743a3b83a8
epp785 <eric_parker@harvard.edu>
2018-07-08 15:13:33 -0400

    Add tests for meals index

---
+++ spec/controllers/meals_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe MealsController do
+  include SessionsHelper
+
+  let(:client) {create :user}
+
+  before do
+    log_in(client)
+  end
+
+  describe "GET index" do
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+end
