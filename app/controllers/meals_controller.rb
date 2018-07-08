86f75637f59da2479ee92c82ab282cf02cabe824
epp785 <eric_parker@harvard.edu>
2018-07-08 15:59:32 -0400

    Search for meal by keys

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,19 @@
 class MealsController < ApplicationController
 
   def index
-    @meals = Meal.all
+    keys = params[:report_form][:q] if params[:report_form]
+    @meals = Meal.joins(:cuisine).includes(:cuisine)
+                 .joins(:dietary_accommodation).includes(:dietary_accommodation)
+                 .joins(:chef).includes(:chef)
+                 .where(cuisines: {name: keys})
+                 .or(Meal.joins(:cuisine).includes(:cuisine)
+                         .joins(:dietary_accommodation).includes(:dietary_accommodation)
+                         .joins(:chef).includes(:chef)
+                         .where(dietary_accommodations: {name: keys}))
+                 .or(Meal.joins(:cuisine).includes(:cuisine)
+                         .joins(:dietary_accommodation).includes(:dietary_accommodation)
+                         .joins(:chef).includes(:chef)
+                         .where(short_name: keys))
   end
 
 end
