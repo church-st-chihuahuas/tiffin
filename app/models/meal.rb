fde0ea19dbb51bb6742a37bdd138114ae9836d2e
Samir Reddigari <samirreddigari@gmail.com>
2018-07-11 19:12:55 -0400

    Fix meal edit and creation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,10 +1,16 @@
 class Meal < ApplicationRecord
-  validates :short_name, :description, :active,
+  validates :short_name, :description,
             presence: true
+
+  validates :active,
+            inclusion: {
+                in: [true, false],
+                message: "can't be blank"
+            }
 
   validates :price,
             presence: true,
-            numericality:   { greater_than_or_equal_to: 0 }
+            numericality: {greater_than_or_equal_to: 0}
 
   belongs_to :chef
   has_and_belongs_to_many :cuisines
